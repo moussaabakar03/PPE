6a9b616ecc12c15aec9bae15ae31f1fb09f33094
moussaabakar03 <alimoussaabakar003@gmail.com>
2026-06-06 03:14:08 +0200

    modification et suppression des paiements par le comptable

--- comptable/urls.py
+++ comptable/urls.py
@@ -5,11 +5,17 @@ app_name = "comptable"
 
 urlpatterns = [
     path('', views.indexComptable, name='indexComptable'),
-    path('listePaiments/', views.listePaiments, name='listePaiments'),
+    path('listePaiements/', views.listePaiements, name='listePaiements'),
     path('selection-salle/<int:id_salle>/', views.selectionSalle, name='selectionSalle'),
     path('selection-classe/', views.selectionClasse, name='selectionClasse'),
     path('liste_eleve/<int:id_salle>/', views.liste_eleve, name='liste_eleve'),
     path('ajouter-paiement/<int:id_inscription>/', views.ajouter_paiement, name='ajouter_paiement'),
+    path('paiement/modifier/<int:id_paiement>/', views.modifier_paiement, name='modifier_paiement'),
+    path('paiement/modif/<int:id_paiement>/', views.modifierPaiement, name='modifierPaiement'),
+
+    path('paiement/suppression/<int:id_paiement>/', views.supprimer_paiement, name='supprimer_paiement'),
+    path('paiement/delete/<int:id_paiement>/', views.supprimerPaiement, name='supprimerPaiement'),
+
     path('enretardSurPaiement/', views.enretardSurPaiement, name='enretardSurPaiement'),
     
     path('alerte-retard-paiement/<int:id_classe>/', views.alerte_retard_paiement, name='alerte_retard_paiement'),
